5aa690bc0694d6cb5d3a8da2dc19227b81937b47
amorris <amorris@aljomo.com>
2024-10-24 13:54:51 -0400

    Added home-manager to nix configuration.

--- flake.nix
+++ flake.nix
@@ -3,72 +3,81 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew}:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager }:
   let
     configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.neovim
-	  pkgs.ripgrep
-	  pkgs.tmux
-	  pkgs.alacritty
-	  pkgs.zsh-completions
-	  pkgs.fzf
-	  pkgs.zoxide
-	  pkgs._1password
-        ];
-	nixpkgs.config.allowUnfree = true;
-	homebrew = {
-        enable = true;
-        brews = [
-          "mas"
-        ];
-        casks = [
-        ];
-        masApps = {
-        };
-        onActivation.cleanup = "zap";
-        onActivation.autoUpdate = true;
-        onActivation.upgrade = true;
-      };
-      
-      system.defaults = {
-        dock.autohide = false;
-        menuExtraClock.Show24Hour = true;
-        dock.orientation = "right";
-        dock.magnification = true;
-      };
+# List packages installed in system profile. To search by name, run:
+# $ nix-env -qaP | grep wget
+	    environment.systemPackages =
+		    [ 
+		    pkgs.neovim
+			    pkgs.ripgrep
+			    pkgs.tmux
+			    pkgs.alacritty
+			    pkgs.zsh-completions
+			    pkgs.fzf
+			    pkgs.zoxide
+			    pkgs._1password
+		    ];
+	    nixpkgs.config.allowUnfree = true;
+	    homebrew = {
+		    enable = true;
+		    brews = [
+			    "mas"
+		    ];
+		    casks = [
+		    ];
+		    masApps = {
+		    };
+		    onActivation.cleanup = "zap";
+		    onActivation.autoUpdate = true;
+		    onActivation.upgrade = true;
+	    };
 
-	 
+	    system.defaults = {
+		    dock.autohide = false;
+		    menuExtraClock.Show24Hour = true;
+		    dock.orientation = "right";
+		    dock.magnification = true;
+		    finder.FXPreferredViewStyle = "clmv";
+	    };
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
+# Auto upgrade nix package and the daemon service.
+	    services.nix-daemon.enable = true;
+# nix.package = pkgs.nix;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+# Necessary for using flakes on this system.
+	    nix.settings.experimental-features = "nix-command flakes";
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-      system.keyboard.enableKeyMapping = true;
-      system.keyboard.remapCapsLockToControl = true;
+# Create /etc/zshrc that loads the nix-darwin environment.
+	    programs.zsh.enable = true;  # default shell on catalina
+# programs.fish.enable = true;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+# Set Git commit hash for darwin-version.
+		    system.configurationRevision = self.rev or self.dirtyRev or null;
+
+# Used for backwards compatibility, please read the changelog before changing.
+# $ darwin-rebuild changelog
+	    system.stateVersion = 5;
+	    system.keyboard.enableKeyMapping = true;
+	    system.keyboard.remapCapsLockToControl = true;
+
+# The platform the configuration will be used on.
+	    nixpkgs.hostPlatform = "aarch64-darwin";
+
+
     };
   in
   {
@@ -84,6 +93,12 @@
             user = "amorris";
           };
         } 
+
+	home-manager.darwinModules.home-manager {
+		home-manager.useGlobalPkgs = true;
+		home-manager.useUserPackages = true;
+		home-manager.users.amorris = import ./home.nix;
+	}
       ];
     };
 
